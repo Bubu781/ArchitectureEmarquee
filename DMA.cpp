1cb936886fef569246c67909ee8995dbfd5c14fe
Adrien <adrien.buot@gmail.com>
2020-10-29 11:06:44 +0100

    changed a lot

--- DMA.cpp
+++ DMA.cpp
@@ -1,7 +1,31 @@
-#include "DAM.hpp"
+#include "DMA.hpp"
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
 
-int main(int argc, const char** argv) {
-    DirectMemoryAccess DMA(0x40000000,0x0,0x20000000);
-    DMA.ready();
-    return 0;
+int main()
+{
+    DirectMemoryAccess DMA(0x40400000,0x01000000,0x01100000);
+
+    //On stoppe le DMA
+    DMA.reset();
+    DMA.halt();
+
+   //Ecriture des 4 bits pour le hardware accelerator
+   DMA.writeSource("0b00000000"); //0
+   DMA.writeSource("0b00001010"); //10
+   DMA.writeSource("0b00000101"); //5
+   DMA.writeSource("0b00000000"); //0
+   DMA.writeSource("Hello");
+
+   //Configuration de l'interruption
+   DMA.setInterrupt(true,true,true,0xFF,10);
+   DMA.ready();
+   
+   //Configuration des adresses pour envoyer et écrire les données sur la dram
+   DMA.setSourceAddress(0x01000000);
+   DMA.setDestinationAddress(0x01100000);
+   
+return 0;
 }

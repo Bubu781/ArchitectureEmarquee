d989a82e090135de70d653ed698e33bb70bf273d
Adrien <adrien.buot@gmail.com>
2020-12-07 18:55:16 +0100

    accelerator v2

--- accelerator.cpp
+++ accelerator.cpp
@@ -11,19 +11,26 @@ typedef​ ​struct​ ​ap_axi​ {
 void​ ​hardware_accelerator​(ap_axi IN[32], ap_axi OUT[16]){
     // Define RTL interface
     // "HLS INTERFACE AXIS" define an AXI-Stream bus
+
     #pragma HLS INTERFACE AXIS port=IN
     // "HLS INTERFACE AXIS" define an AXI-Stream bus
     #pragma HLS INTERFACE AXIS port=OUT
     int j=0;
-    for​(​int​ i=​1; i<32; i+=2) {
-        // Add a value to the initial data and send it back to the DMA
-        OUT[j].data = (ap_int) (IN[i].data + ​IN[i-1].data)/2​;
+    ap_int<​32​> in_values[32];
+    for​(​int​ i=​0; i<32; i++) {
+        in_values[i] = IN[i].data;
         ​// Always copy keep signal from input so you
         // do not have to manage it
-        OUT[j].keep = IN[i].keep;
-        ​// Always copy last signal from input so you
-        // do not have to manage it
-        OUT[j].last = IN[i].last;
+        if(i < 16){
+            OUT[i].keep = IN[i].keep;
+            ​// Always copy last signal from input so you
+            // do not have to manage it
+            OUT[i].last = IN[i].last;
+        }
+    }
+    for(int i=1; i < 32; i+= 2){
+        // Add a value to the initial data and send it back to the DMA
+        OUT[j].data = (ap_int) (in_values[i] + in_values[i-1])/2​;
         j++;
-    } 
+    }
 }

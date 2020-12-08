aaf221c4201c1611020a7f083724f1bb4e8aaa88
Adrien <adrien.buot@gmail.com>
2020-12-08 18:21:20 +0100

    J'ai essayé de blinder à fond

--- accelerator.cpp
+++ accelerator.cpp
@@ -14,22 +14,22 @@ void​ ​hardware_accelerator​(ap_axi IN[8], ap_axi OUT[4]){
     #pragma HLS INTERFACE AXIS port=IN
     // "HLS INTERFACE AXIS" define an AXI-Stream bus
     #pragma HLS INTERFACE AXIS port=OUT
-    int j=0;
+    int j=0, val1, val2, val3, val4;
     ap_uint<32> value, value1, value2;
     for​(​int​ i=​0; i<8; i++) {
-        int val1 = (IN[i].data & 0xFF000000) >> 24;
-        int val2 = (IN[i].data & 0x00FF0000) >> 16;
-        int val3 = (IN[i].data & 0x0000FF00) >> 8;
-        int val4 = (IN[i].data & 0x000000FF);
+        val1 = (IN[i].data & 0xFF000000) >> 24;
+        val2 = (IN[i].data & 0x00FF0000) >> 16;
+        val3 = (IN[i].data & 0x0000FF00) >> 8;
+        val4 = (IN[i].data & 0x000000FF);
         if(i % 2 == 0){
             value1 = (ap_uint<32>) (val1 + val2)/2​;
             value2 = (ap_uint<32>) (val3 + val4)/2​;
-            value = value1 << 24 | value2 << 16;
+            value = (value1 << 24 | value2 << 16) & 0xFFFF0000;
         }else{
-            value1 = (ap_uint<32>) (val1 + val2)/2​;
-            value2 = (ap_uint<32>) (val3 + val4)/2​;
+            value1 = (ap_uint<32>) ((val1 + val2)/2​) & 0x000000FF;
+            value2 = (ap_uint<32>) ((val3 + val4)/2​) & 0x000000FF;
             // Add a value to the initial data and send it back to the DMA
-            OUT[j].data = value | value1 << 8 | value2​;
+            OUT[j].data = (ap_uint<32>) value | value1 << 8 | value2​;
             ​// Always copy keep signal from input so you
             // do not have to manage it
             OUT[j].keep = IN[i].keep;

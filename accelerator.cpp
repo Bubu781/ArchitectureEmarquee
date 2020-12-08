faf5cc547eb61ff85af6a2fdd1ee1bb424a05678
Adrien <adrien.buot@gmail.com>
2020-12-08 11:26:04 +0100

    V5

--- accelerator.cpp
+++ accelerator.cpp
@@ -1,27 +1,27 @@
 #include "ap_axi_sdata.h"
 
 typedef​ ​struct​ ​ap_axi​ { 
-    ap_uint<8> data; ​// Represents data you wrote on the DRAM
+    ap_uint<32> data; ​// Represents data you wrote on the DRAM
     ap_uint<​4​> keep;​ // Used when words are longer than 32 bits
     ap_uint<​1​> last; ​// Assert the last data of the burst
 } ap_axi;
 
 // This accelerator read 10 int (32 bits each) and
 // return 10 int (32 bits each)
-void​ ​hardware_accelerator​(ap_axi IN[32], ap_axi OUT[16]){
+void​ ​hardware_accelerator​(ap_axi IN[8], ap_axi OUT[4]){
     // Define RTL interface
     // "HLS INTERFACE AXIS" define an AXI-Stream bus
     #pragma HLS INTERFACE AXIS port=IN
     // "HLS INTERFACE AXIS" define an AXI-Stream bus
     #pragma HLS INTERFACE AXIS port=OUT
     int j=0;
-    ap_uint<8> value;
-    for​(​int​ i=​0; i<32; i++) {
+    ap_uint<32> value;
+    for​(​int​ i=​0; i<8; i++) {
         if(i % 2 == 0){
             value = IN[i].data;
         }else{
             // Add a value to the initial data and send it back to the DMA
-            OUT[j].data = (ap_uint<8>) (IN[i].data + value)/2​;
+            OUT[j].data = (ap_uint<32>) (IN[i].data + value)/2​;
             ​// Always copy keep signal from input so you
             // do not have to manage it
             OUT[j].keep = IN[i].keep;

a180991255012f444d932679d857a474b2d64b4e
Adrien <adrien.buot@gmail.com>
2020-12-10 16:01:17 +0100

    use IN.data once

--- accelerator.cpp
+++ accelerator.cpp
@@ -16,16 +16,18 @@ void​ ​hardware_accelerator​(ap_axi IN[8], ap_axi OUT[4]){
     #pragma HLS INTERFACE AXIS port=OUT
     unsigned short int  j=0;
     ap_uint<32> value;
+    ap_uint<32> data;
     for​(unsigned short int i=​0; i<8; i++) {
+        data = IN[i].data;
         if(i % 2 == 0){
             // Apply a mask to have 4 distincts pixels
             // Calculate the average of the pixels at the same position
             //We set the first 2 new pixels calculated
-            value = (ap_uint<32>) (((((IN[i].data & 0xFF000000) >> 24) + ((IN[i].data & 0xFF0000) >> 16))/2) << 8 | ((((IN[i].data & 0xFF00) >> 8) + (IN[i].data & 0xFF))/2));
+            value = (ap_uint<32>) (((((data & 0xFF000000) >> 24) + ((data & 0xFF0000) >> 16))/2) << 8 | ((((data & 0xFF00) >> 8) + (data & 0xFF))/2));
         }else{
             // Add a value to the initial data and send it back to the DMA
             // We send 4 new pixels 
-            OUT[j].data = (ap_uint<32>) (value << 16 | (((((IN[i].data & 0xFF000000) >> 24) + ((IN[i].data & 0xFF0000) >> 16))/2) << 8 | ((((IN[i].data & 0xFF00) >> 8) + (IN[i].data & 0xFF))/2)));
+            OUT[j].data = (ap_uint<32>) (value << 16 | (((((data & 0xFF000000) >> 24) + ((data & 0xFF0000) >> 16))/2) << 8 | ((((data & 0xFF00) >> 8) + (data & 0xFF))/2)));
             ​// Always copy keep signal from input so you
             // do not have to manage it
             OUT[j].keep = IN[i].keep;
